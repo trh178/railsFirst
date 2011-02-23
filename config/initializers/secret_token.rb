10b6ab0482d99bb008072caaf0cfeb0575ac838e
root <root@toddarch.localdomain>
2011-02-22 21:24:34 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+First::Application.config.secret_token = '1a5ae1220f890c4deb92968df36638335b9cbd61c9da6561a1a55fbf9a0e6ea3a888c778ca5f1fa68f139faa6bedc2f3d51f972a5e63587ee18f139a6cca60bb'
