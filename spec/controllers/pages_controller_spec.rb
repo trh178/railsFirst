249a14fe026e36cde858271d7cc6c979dad4d054
trh178 <trh178@gmail.com>
2011-02-28 19:37:42 -0500

    done with static page testing

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,6 +8,11 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+
+    it "should have correct title" do
+      get 'home'
+      response.should have_selector("title", :content => "| Home")
+    end
   end
 
   describe "GET 'contact'" do
@@ -15,12 +20,22 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    it "should have correct title" do
+      get 'contact'
+      response.should have_selector("title", :content => "| Contact")
+    end
   end
 
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
+    end
+
+    it "should have correct title" do
+      get 'about'
+      response.should have_selector("title", :content => "| About")
     end
   end
 
