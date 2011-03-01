9b335ad28fcfc2ae802300415b555acb796b3516
trh178 <trh178@gmail.com>
2011-02-28 20:28:08 -0500

    added application helper function to enforce DRY

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # return a title on a per page basis
+  def title
+    base_title = "Ruby on Rails - First App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
