ad255cc59d95651baa542d8b9797b74debe10139
trh178 <trh178@gmail.com>
2011-03-08 20:07:59 -0500

    finished signup page routing and testing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  # return logo
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
 
   # return a title on a per page basis
   def title
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -22,4 +22,25 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Help")
   end
 
+  it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign Up")
+  end
+
+
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+    click_link "Help"
+    response.should have_selector('title', :content => "Help")
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")
+    click_link "Home"
+    response.should have_selector('title', :content => "Home")
+    click_link "Sign Up Now!"
+    response.should have_selector('title', :content => "Sign Up")
+  end
+
 end
